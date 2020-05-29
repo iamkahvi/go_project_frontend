9c863a187d92b084a218327d8cd06b6cecba6311
Kahvi Patel <iamkahvi@gmail.com>
2020-05-28 23:14:15 -0700

    KP: trying to use this library effectively

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { User } from "../../types";
+import { CSSTransition } from "react-transition-group";
 
 interface Props {
   isFetching: boolean;
@@ -16,16 +17,20 @@ export default function UserList(props: Props) {
         {isFetching ? (
           "Loading..."
         ) : (
-          <>
-            {users.map((user, key) => (
-              <li key={key}>
-                <div className="item">
-                  {user.Name}
-                  <button onClick={() => handleDelete(user.ID)}>delete</button>
-                </div>
-              </li>
-            ))}
-          </>
+          <CSSTransition classNames="example" in={true} timeout={300}>
+            <>
+              {users.map((user, key) => (
+                <li key={key}>
+                  <div className="item">
+                    {user.Name}
+                    <button onClick={() => handleDelete(user.ID)}>
+                      delete
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </>
+          </CSSTransition>
         )}
       </ul>
     </div>
