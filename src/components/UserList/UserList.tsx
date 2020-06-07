b856d42c2d7fe5006f998ae5877d14b881b3f6ff
Kahvi Patel <iamkahvi@gmail.com>
2020-06-06 21:30:46 -0700

    KP: adding css module support

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { User } from "../../types";
 import { CSSTransition } from "react-transition-group";
+import * as styles from "./userlist.module.scss";
 
 interface Props {
   isFetching: boolean;
@@ -17,20 +18,16 @@ export default function UserList(props: Props) {
         {isFetching ? (
           "Loading..."
         ) : (
-          <CSSTransition classNames="example" in={true} timeout={300}>
-            <>
-              {users.map((user, key) => (
-                <li key={key}>
-                  <div className="item">
-                    {user.Name}
-                    <button onClick={() => handleDelete(user.ID)}>
-                      delete
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </>
-          </CSSTransition>
+          <div className={styles.list}>
+            {users.map((user, key) => (
+              <li key={key}>
+                <div className="item">
+                  {user.Name}
+                  <button onClick={() => handleDelete(user.ID)}>delete</button>
+                </div>
+              </li>
+            ))}
+          </div>
         )}
       </ul>
     </div>
