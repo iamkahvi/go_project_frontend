d92053e03141ac360e4b28e16ca1170fc4e16de8
Kahvi Patel <iamkahvi@gmail.com>
2020-06-08 18:28:24 -0700

    KP: adding layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Layout from "./components/Layout/Layout";
 
 import "./styles/App.scss";
 
 const App = () => (
   <Switch>
-    <Route exact={true} path="/" component={Home} />
+    <Layout>
+      <Route exact={true} path="/" component={Home} />
+    </Layout>
   </Switch>
 );
 
